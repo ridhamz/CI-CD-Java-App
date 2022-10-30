2110008bc8003e3c41b510972dd3dc0c0fca0d3f
Ridha Mezrigui <47607249+ridhamz@users.noreply.github.com>
2022-10-30 19:31:04 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t ridhamezrigui/java-app:2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push nanajanashia/demo-app:jma-2.0'
     }
